aea54da6961bfdad1a8e80e885710bdce28e830f
OlegKotov <olegkotov003@gmail.com>
2021-12-19 00:56:08 +0300

    add css AlfaDigital-Visualization blocks

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
-import "./import/modules";
-$(".slick-slider").slick({
-    slidesToShow: 3,
-    infinite: false,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000
-        // dots: false, Boolean
-        // arrows: false, Boolean
-});
+// import "./import/modules";
+// $(".slick-slider").slick({
+//     slidesToShow: 3,
+//     infinite: false,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000
+//         // dots: false, Boolean
+//         // arrows: false, Boolean
+// });
 
 
 // Image Slider Demo:
