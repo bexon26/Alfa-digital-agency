c061c1acd118779a90150af1bc271ac8f151af09
bexon26 <bexonn80@gmail.com>
2021-12-21 12:36:30 +0300

    fix: slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,25 @@
 import "./import/modules";
-import $ from 'jquery';
+// import $ from 'jquery';
+import * as $ from 'jquery';
+// import 'slick-carousel';
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-theme.css';
 // let ir = $(".slick-slider")
-function initSlider() {
-   $(".slick-slider").slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      arrows: false,
-      dots: true, Boolean
-      // arrows: false, Boolean
-   });
-}
+
+// $(".slick-slider").slick({
+//    slidesToShow: 3,
+//    slidesToScroll: 1,
+//    autoplay: true,
+//    autoplaySpeed: 2000,
+//    arrows: false,
+//    dots: true, Boolean
+//    // arrows: false, Boolean
+// });
+
 
 // Image Slider Demo:
 // https://codepen.io/vone8/pen/gOajmOo
 
-$(document).on('ready', function () {
-   initSlider();
-});
+// $(document).on('ready', function () {
+//    initSlider();
+// });
